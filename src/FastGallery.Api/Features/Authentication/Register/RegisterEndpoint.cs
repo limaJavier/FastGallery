cff823efc746b8521aa74a52039622a5e237f150
Javier Lima <javi.limashelby@gmail.com>
2024-01-22 15:00:04 -0500

    Implement RegisterEndpoint

--- src/FastGallery.Api/Features/Authentication/Register/RegisterEndpoint.cs
+++ src/FastGallery.Api/Features/Authentication/Register/RegisterEndpoint.cs
@@ -1,11 +1,20 @@
 using FastEndpoints;
+using FastGallery.Application.Authentication.Commands.Register;
 using FastGallery.Application.Authentication.Common;
 using FastGallery.Contracts.Authentication;
+using MediatR;
 
 namespace FastGallery.Api.Features.Authentication.Register;
 
 public class RegisterEndpoint : Endpoint<RegisterRequest, AuthenticationResult>
 {
+    private readonly ISender _mediator;
+
+    public RegisterEndpoint(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
     public override void Configure()
     {
         Post("/auth/register");
@@ -14,10 +23,17 @@ public class RegisterEndpoint : Endpoint<RegisterRequest, AuthenticationResult>
 
     public override async Task HandleAsync(RegisterRequest request, CancellationToken cancellationToken)
     {
-        var result = new AuthenticationResult(
+        // TODO: Use a mapper to create command
+        // Create command
+        var command = new RegisterCommand(
+            request.FirstName,
+            request.LastName,
             request.Email,
-            "someToken"
+            request.Password
         );
+
+        var result = await _mediator.Send(command);
+        
         await SendAsync(result, statusCode: 200);
     }
 }
