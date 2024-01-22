45dca294ecce93986796658473af9e081af1ff83
Javier Lima <javi.limashelby@gmail.com>
2024-01-22 14:17:03 -0500

    Define User Domain-Entity

---
+++ src/FastGallery.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace FastGallery.Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
