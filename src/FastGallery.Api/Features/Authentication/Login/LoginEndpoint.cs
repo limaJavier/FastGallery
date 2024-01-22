30ef6c20f1ff0e51c70e2230f0918a1e589eff08
Javier Lima <javi.limashelby@gmail.com>
2024-01-22 15:42:43 -0500

    Implement LoginEndpoint

--- src/FastGallery.Api/Features/Authentication/Login/LoginEndpoint.cs
+++ src/FastGallery.Api/Features/Authentication/Login/LoginEndpoint.cs
@@ -1,11 +1,20 @@
 using FastEndpoints;
 using FastGallery.Application.Authentication.Common;
+using FastGallery.Application.Authentication.Queries.Login;
 using FastGallery.Contracts.Authentication;
+using MediatR;
 
 namespace FastGallery.Api.Features.Authentication.Register;
 
 public class LoginEndpoint : Endpoint<LoginRequest, AuthenticationResult>
 {
+    private readonly ISender _mediator;
+
+    public LoginEndpoint(ISender mediator)
+    {
+        _mediator = mediator;
+    }
+
     public override void Configure()
     {
         Post("/auth/login");
@@ -14,10 +23,13 @@ public class LoginEndpoint : Endpoint<LoginRequest, AuthenticationResult>
 
     public override async Task HandleAsync(LoginRequest request, CancellationToken cancellationToken)
     {
-        var result = new AuthenticationResult(
+        var query = new LoginQuery(
             request.Email,
-            "someToken"
+            request.Password
         );
+
+        var result = await _mediator.Send(query);
+
         await SendAsync(result, statusCode: 200);
     }
 }
