c7e92270d312bb107e6ee94fa9c51c9d3c4fec24
Javier Lima <javi.limashelby@gmail.com>
2024-01-22 14:59:53 -0500

    Inject Application and Infrastructure projects on API

--- src/FastGallery.Api/Program.cs
+++ src/FastGallery.Api/Program.cs
@@ -1,9 +1,14 @@
+using FastGallery.Application;
+using FastGallery.Infrastructure;
 using FastEndpoints;
 using FastEndpoints.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddFastEndpoints().SwaggerDocument();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
 
 var app = builder.Build();
 
