01d25c83864839e41c060b733904985fc6951def
Javier Lima <javi.limashelby@gmail.com>
2024-01-22 20:55:44 -0500

    Add Authentication into Dependency Injection

--- src/FastGallery.Infrastructure/DependencyInjection.cs
+++ src/FastGallery.Infrastructure/DependencyInjection.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using FastGallery.Application.Interfaces.Authentication;
 using FastGallery.Application.Interfaces.Persistence;
 using FastGallery.Infrastructure.Authentication;
 using FastGallery.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FastGallery.Infrastructure;
 
@@ -11,9 +15,32 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfigurationManager configuration)
     {
-        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
-        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SECTION_NAME));
+        // services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        // services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SECTION_NAME));
+        services.AddAuth(configuration);
         services.AddSingleton<IUserRepository, UserRepository>();
+        return services;
+    }
+
+    private static IServiceCollection AddAuth(this IServiceCollection services, IConfigurationManager configuration)
+    {
+        var jwtSettings = new JwtSettings();
+        configuration.Bind(JwtSettings.SECTION_NAME, jwtSettings);
+
+        services.AddSingleton(Options.Create(jwtSettings));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
+        services.AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidAudience = jwtSettings.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+        });
+
         return services;
     }
 }
